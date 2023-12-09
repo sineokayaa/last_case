844fbae37413d771996e10afc97e7b2284ac53ce
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-12-09 15:55:27 +0700

    12

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import os
 import RU_LOCAL as RU
 
+
+def acceptCommand():
+    command = input(RU.QUESTION_1)
+    flag = False
+    while not flag:
+        try:
+            command = int(command)
+            if 1 <= command <= 7:
+                flag = True
+                return command
+            return RU.MISTAKE_1
+        except ValueError:
+            return RU.MISTAKE_2
+
+
 def main():
     while True:
         print(os.getcwd())
@@ -11,5 +26,6 @@ def main():
             print('Работа программы завершена.')
             break
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == '__main__':
+    main()
