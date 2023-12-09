41a491367f4fe21b98444a511afd8b470c42193d
sineokayaa <a.sineokaya@g.nsu.ru>
2023-12-09 15:36:55 +0700

    2

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-jshs
+import os
+import RU_LOCAL as RU
+
+def main():
+    while True:
+        print(os.getcwd())
+        print(RU.MENU)
+        command = acceptCommand()
+        runCommand(command)
+        if command == QUIT:
+            print('Работа программы завершена.')
+            break
+
+    if __name__ == '__main__':
+        main()
